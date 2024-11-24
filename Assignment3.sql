84a57435b5f47b36d16ab85cfc6149850316aad8
Sahil <sahil.quliyev.02@gmail.com>
2024-11-25 00:59:08 +0400

    Applied 1NF to authors column by separating author names with CREATE TABLE AS

--- Assignment3.sql
+++ Assignment3.sql
@@ -17,25 +17,12 @@ CREATE TABLE Unnormalized1 (
 
 \copy Unnormalized1 FROM '/Users/sahil/Desktop/Unnormalized1.csv' WITH (FORMAT csv, HEADER true, DELIMITER ';');
 
-CREATE TABLE first_normal_form (
-    CRN INT,
-    ISBN VARCHAR(255) NOT NULL,
-    Title VARCHAR(255),
-    Author VARCHAR(255),
-    Edition VARCHAR(50),
-    Publisher VARCHAR(255),
-    Publisher_address VARCHAR(255),
-    Pages INT,
-    Year INT,
-    Course_name VARCHAR(255)
-);
-
-INSERT INTO first_normal_form (CRN, ISBN, Title, Author, Edition, Publisher, Publisher_address, Pages, Year, Course_name)
+CREATE TABLE first_normal_form AS
 SELECT
     CRN,
     ISBN,
     Title,
-	unnest(string_to_array(REGEXP_REPLACE(Authors, '[^[:print:]]', ' ', 'g'), ', ')) AS Author, -- Replace non-printing characters with spaces
+    unnest(string_to_array(REGEXP_REPLACE(Authors, '[^[:print:]]', ' ', 'g'), ', ')) AS Author,  -- Replace non-printing characters with spaces
     Edition,
     Publisher,
     Publisher_address,
@@ -43,6 +30,5 @@ SELECT
     Year,
     Course_name
 FROM Unnormalized1;
-
 SELECT * FROM first_normal_form;
 
